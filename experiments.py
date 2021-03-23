0c18884158d03c88ce89652024ec261c4cf14ee5
Tamir <tamirshashafb@gmail.com>
2021-03-23 10:48:27 +0200

    some shit

--- experiments.py
+++ experiments.py
@@ -20,14 +20,18 @@ class Experiment:
                  noise_std=0.3,
                  use_exact_signal_power=False,
                  use_exact_k=False,
+                 save=False,
                  logs=False):
         self._n = n  # Data length
         self._d = d  # Signal Length
         self._k = k  # Num of signal occurrences
         self._signal_avg_power = signal_avg_power  # Signal Expected Avg Power
         self._signal_seperation = signal_seperation
+        self._use_exact_signal_power = use_exact_signal_power
+        self._use_exact_k = use_exact_k
         self._noise_mean = noise_mean  # Expected noise mean
         self._noise_std = noise_std  # Expected noise std
+        self._save = save
         self._logs = logs
         self._results = {}
 
@@ -38,13 +42,13 @@ class Experiment:
         self._noisy_y = add_gaus_noise(self._y_with_signals, self._noise_mean, self._noise_std)
         self._y = self._noisy_y
 
-        self._signal_length_options = np.arange(self._d // 4, int(self._d * 2), 3)
+        self._signal_length_options = np.arange(self._d // 4, int(self._d * 2), 4)
 
         exp_attr = {
             "d": self._d,
             "k": self._k,
-            "use_exact_signal_power": use_exact_signal_power,
-            "use_exact_k": use_exact_k
+            "use_exact_signal_power": self._use_exact_signal_power,
+            "use_exact_k": self._use_exact_k
         }
         self._length_extractor = LengthExtractor(y=self._y,
                                                  length_options=self._signal_length_options,
@@ -69,8 +73,13 @@ class Experiment:
 
     def plot_results(self):
         plt.title(f"N={self._n}, D={self._d}, K={self._k}, NoiseMean={self._noise_mean}, NoiseSTD={self._noise_std} "
+                  f" Seperation={self._signal_seperation}, ExactPower={self._use_exact_signal_power}, ExactK={self._use_exact_k}\n"
                   f"\n Most likely D={self._results['d']}, Took {'%.3f' % (self._results['total_time'])} Seconds")
         plt.plot(self._signal_length_options, self._results['likelihoods'])
+
+        if self._save:
+            plt.savefig('experiments_results/' + str(time.time()) + '.pdf')
+
         plt.show()
 
 
@@ -112,18 +121,20 @@ def __main__():
     # experiment3.plot_results()
 
     experiment4 = Experiment(
-        n=6000,
-        d=30,
-        k=100,
+        n=10000,
+        d=50,
+        k=70,
         signal_avg_power=1,
-        signal_seperation=10,
+        signal_seperation=0,
         noise_mean=0,
         noise_std=1.5,
+        use_exact_k=False,
         use_exact_signal_power=True,
-        logs=True
+        logs=True,
+        save=True
     )
-    # experiment4.run()
-    # experiment4.plot_results()
+    experiment4.run()
+    experiment4.plot_results()
 
 
 if __name__ == '__main__':
