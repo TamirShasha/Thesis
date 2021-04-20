1cfffdb7b22c23dcccae5ecbf3cf2766319ae2d1
itaysason <itaysason@mail.tau.ac.il>
2021-04-20 18:01:41 +0300

    logsumexp supports axis and keepdims

--- src/utils/logsumexp.py
+++ src/utils/logsumexp.py
@@ -3,7 +3,7 @@ import numba as nb
 
 
 @nb.jit
-def logsumexp(a):
+def logsumexp(a, axis=None, keepdims=False):
     """
     To reduce running time I implemented logsumexp myself, the scipy version has too much additional things I don't need
     :param a:
@@ -11,9 +11,10 @@ def logsumexp(a):
     :param keepdims:
     :return:
     """
-    a_max = np.max(a)
-    if np.isneginf(a_max):
-        return -np.inf
-    output = np.log(np.sum(np.exp(a - a_max)))
+    a_max = np.max(a, axis=axis, keepdims=True)
+    a_max[np.isneginf(a_max)] = 0
+    output = np.log(np.sum(np.exp(a - a_max), axis=axis, keepdims=keepdims))
+    if not keepdims:
+        a_max = np.squeeze(a_max, axis=axis)
     output += a_max
     return output
