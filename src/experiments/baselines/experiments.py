03440101deff69fcdc345569ee86199143fe3a9c
Tamir <tamirshashafb@gmail.com>
2021-06-16 18:03:59 +0300

    baselines

--- src/experiments/baselines/experiments.py
+++ src/experiments/baselines/experiments.py
@@ -77,63 +77,100 @@ def ellipse12(d, p):
 """
 2D Disk Experiments with noise std 3,5,8,10,12,15
 """
-# run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_3std', Shapes2D.disk, 3, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_3std', Shapes2D.disk, 3, EstimationMethod.WellSeparation)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_5std', Shapes2D.disk, 5, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_5std', Shapes2D.disk, 5, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_8std', Shapes2D.disk, 8, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_8std', Shapes2D.disk, 8, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_10std', Shapes2D.disk, 10, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_10std', Shapes2D.disk, 10, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_12std', Shapes2D.disk, 12, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_12std', Shapes2D.disk, 12, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_disk_300_curves_15std', Shapes2D.disk, 15, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_disk_300_vws_15std', Shapes2D.disk, 15, EstimationMethod.WellSeparation)
+for std in [3, 5, 8, 10, 12, 15]:
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_curves_{std}std', Shapes2D.disk, std, EstimationMethod.Curves)
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_vws_{std}std', Shapes2D.disk, std,
+                               EstimationMethod.WellSeparation)
 
 """
 2D Ellipse 2:3 Experiments with noise std 3,5,8,10,12,15
 """
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_3std', ellipse23, 3, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_3std', ellipse23, 3, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_5std', ellipse23, 5, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_5std', ellipse23, 5, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_8std', ellipse23, 8, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_8std', ellipse23, 8, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_10std', ellipse23, 10, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_10std', ellipse23, 10, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_12std', ellipse23, 12, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_12std', ellipse23, 12, EstimationMethod.WellSeparation)
-
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_curves_15std', ellipse23, 15, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse23_300_vws_15std', ellipse23, 15, EstimationMethod.WellSeparation)
+for std in [3, 5, 8, 10, 12, 15]:
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_curves_{std}std', ellipse23, std, EstimationMethod.Curves)
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_vws_{std}std', ellipse23, std,
+                               EstimationMethod.WellSeparation)
 
 """
 2D Ellipse 1:2 Experiments with noise std 3,5,8,10,12,15
 """
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_3std', ellipse12, 3, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_3std', ellipse12, 3, EstimationMethod.WellSeparation)
+for std in [3, 5, 8, 10, 12, 15]:
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_curves_{std}std', ellipse12, std, EstimationMethod.Curves)
+    run_2d_baseline_experiment(f'2D_4000x4000_ellipse12_300_vws_{std}std', ellipse12, std,
+                               EstimationMethod.WellSeparation)
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_5std', ellipse12, 5, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_5std', ellipse12, 5, EstimationMethod.WellSeparation)
+"""
+Shpere Experiments
+"""
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_8std', ellipse12, 8, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_8std', ellipse12, 8, EstimationMethod.WellSeparation)
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_10std', ellipse12, 10, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_10std', ellipse12, 10, EstimationMethod.WellSeparation)
+def run_2d_baseline_sphere_experiment(exp_name, noise_std, method):
+    """
+    baseline
+    """
+    sim_data = DataSimulator2D(rows=4000,
+                               columns=4000,
+                               signal_length=300,
+                               signal_power=1,
+                               signal_fraction=1 / 8,
+                               signal_gen=Shapes2D.sphere,
+                               noise_std=noise_std,
+                               noise_mean=0)
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_12std', ellipse12, 12, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_12std', ellipse12, 12, EstimationMethod.WellSeparation)
+    print('#### START NEW EXPERIMENT ####')
+    Experiment2D(
+        name=exp_name,
+        simulator=sim_data,
+        estimation_method=method,
+        signal_power_estimator_method=SignalPowerEstimator.FirstMoment,
+        length_options=np.arange(50, 500, 10),
+        signal_num_of_occurrences_boundaries=(20, 200),
+        signal_area_coverage_boundaries=(0.05, 0.3),
+        num_of_power_options=10,
+        plot=False,
+        save=True,
+        save_dir=os.path.join(ROOT_DIR, f'src/experiments/baselines/plots/')
+    ).run()
+    print('#### DONE EXPERIMENT ####')
 
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_curves_15std', ellipse12, 15, EstimationMethod.Curves)
-run_2d_baseline_experiment('2D_4000x4000_ellipse12_300_vws_15std', ellipse12, 15, EstimationMethod.WellSeparation)
+
+for std in [3, 5, 8, 10, 12, 15]:
+    run_2d_baseline_sphere_experiment(f'2D_4000x4000_sphere_300_curves_{std}std', std, EstimationMethod.Curves)
+    run_2d_baseline_sphere_experiment(f'2D_4000x4000_sphere_300_vws_{std}std', std, EstimationMethod.WellSeparation)
+
+
+def run_2d_baseline_sphere_ctf_experiment(exp_name, noise_std, method):
+    """
+    baseline
+    """
+    sim_data = DataSimulator2D(rows=4000,
+                               columns=4000,
+                               signal_length=300,
+                               signal_power=10,
+                               signal_fraction=1 / 8,
+                               signal_gen=Shapes2D.sphere,
+                               noise_std=noise_std,
+                               noise_mean=0,
+                               apply_ctf=True)
+
+    print('#### START NEW EXPERIMENT ####')
+    Experiment2D(
+        name=exp_name,
+        simulator=sim_data,
+        estimation_method=method,
+        signal_power_estimator_method=SignalPowerEstimator.FirstMoment,
+        length_options=np.arange(50, 500, 10),
+        signal_num_of_occurrences_boundaries=(20, 200),
+        signal_area_coverage_boundaries=(0.05, 0.3),
+        num_of_power_options=10,
+        plot=False,
+        save=True,
+        save_dir=os.path.join(ROOT_DIR, f'src/experiments/baselines/plots/')
+    ).run()
+    print('#### DONE EXPERIMENT ####')
+
+
+for std in [3, 5, 8, 10, 12, 15]:
+    run_2d_baseline_sphere_ctf_experiment(f'2D_4000x4000_sphere_300_curves_{std}std', std, EstimationMethod.Curves)
+    run_2d_baseline_sphere_ctf_experiment(f'2D_4000x4000_sphere_300_vws_{std}std', std, EstimationMethod.WellSeparation)
