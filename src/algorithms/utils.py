c67af6052a56aefd9280b624ade9aa119d5eba55
itaysason <itaysason@mail.tau.ac.il>
2021-07-13 16:27:05 +0300

    Cleaned code, added option for computing many derivatives

--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -106,20 +106,6 @@ def crop(x, out_shape):
     return out
 
 
-def gradient_descent(F_F_tag, initial_x, t=0.01, epsilon=1e-10, max_iter=200):
-    x_prev = initial_x
-    F_prev, F_tag_prev = F_F_tag(x_prev)
-    for i in range(max_iter):
-        x_current = x_prev + t * F_tag_prev
-        F_current, F_tag_current = F_F_tag(x_current)
-        if np.abs(F_current - F_prev) < epsilon:
-            break
-        t = np.abs((x_current - x_prev) / (F_tag_prev - F_tag_current))
-        x_prev, F_prev, F_tag_prev = x_current, F_current, F_tag_current
-    return F_current, x_current
-
-
-
 # Utils regarding likelihood computation
 def log_size_S_1d(n, k, d):
         """
@@ -260,6 +246,124 @@ def dynamic_programming_2d_after_pre_compute(n, k, d, constants):
     return mapping
 
 
+# utils for optimization
+def precompute_f_f_tag(power, mgraph, filt, noise_std):
+    x_tag = np.flip(filt)  # Flipping to cross-correlate
+    if len(x_tag.shape) == 1 and len(mgraph.shape) == 2:
+        conv = np.array([convolve(mgraph[i], x_tag, mode='valid') for i in range(mgraph.shape[0])])
+    else:
+        conv = convolve(mgraph, x_tag, mode='valid')
+    conv /= noise_std ** 2
+    return conv, conv * power
+
+
+@nb.jit
+def dynamic_programming_1d_derivative(n, k, d, consts1, consts2, g=None):
+    """
+    Compute log(\sum_{s in S_(n,k,d)}\prod_{i in s}c_i)
+    :param n:
+    :param k:
+    :param d:
+    :param consts1:
+    :param consts2:
+    :param g:
+    :return:
+    """
+    g = dynamic_programming_1d(n, k, d, consts2) if g is None else g
+
+    r = - np.min(consts1) + 1
+    consts1 = np.log(consts1 + r)
+
+    mapping = np.full((n + 1, k + 1), -np.inf)
+    mapping[:, 0] = 0
+
+    for curr_k in range(1, k + 1):
+        for i in range(n - curr_k * d, -1, -1):
+            mapping[i, curr_k] = np.logaddexp(consts2[i] + mapping[i + d, curr_k - 1], mapping[i + 1, curr_k])
+            mapping[i, curr_k] = np.logaddexp(consts1[i] + consts2[i] + g[i + d, curr_k - 1], mapping[i, curr_k])
+
+    return np.exp(mapping[0, k] - g[0, k]) - k * r
+
+
+@nb.jit
+def dynamic_programming_1d_many_derivative(n, k, d, consts1, consts2, g=None):
+    """
+    Do the 1d dynamic programming for many constant vectors.
+    Output is mapping such that mapping[:, :, i] = dynamic_programming_1d(n, k, d, constants[i])
+    :param n:
+    :param k:
+    :param d:
+    :param constants:
+    :return:
+    """
+
+    g = dynamic_programming_1d_many(n, max(k), d, consts2) if g is None else g
+    g = g.transpose((1, 2, 0)).copy()
+
+    # Changing constants shape so the first axis is continuous
+    consts1 = consts1.T.copy()
+    consts2 = consts2.T.copy()
+
+    r = - np.min(consts1) + 1
+    consts1 = np.log(consts1 + r)
+
+    # Allocating memory
+    # Default is -inf everywhere as there are many places where the probability is 0 (when i > n - k * d)
+    # when k=0 the probability is 1
+    mapping = np.full((n + 1, max(k) + 1, consts1.shape[-1]), -np.inf)
+    mapping[:, 0] = 0
+
+    for curr_k in range(1, max(k) + 1):
+        for i in range(n - curr_k * d, -1, -1):
+            mapping[i, curr_k] = np.logaddexp(consts2[i] + mapping[i + d, curr_k - 1], mapping[i + 1, curr_k])
+            mapping[i, curr_k] = np.logaddexp(consts1[i] + consts2[i] + g[i + d, curr_k - 1], mapping[i, curr_k])
+
+    mapping = mapping.transpose((2, 0, 1))
+    g = g.transpose((2, 0, 1))
+    return np.array([np.exp(mapping[i, 0, k[i]] - g[i, 0, k[i]]) - k[i] * r for i in range(len(k))])
+
+
+def dynamic_programming_2d_derivative(n, k, d, consts1, consts2, g=None):
+    """
+    Compute log(\sum_{s in S_(n,k,d)}\prod_{i in s}c_i)
+    :param n:
+    :param k:
+    :param d:
+    :param consts1:
+    :param consts2:
+    :param g:
+    :return:
+    """
+    g = dynamic_programming_1d(n, k, d, consts2) if g is None else g
+
+    r = - np.min(consts1) + 1
+    consts1 = np.log(consts1 + r)
+
+    mapping = np.full((n + 1, k + 1), -np.inf)
+    mapping[:, 0] = 0
+
+    for curr_k in range(1, k + 1):
+        for i in range(n - curr_k * d, -1, -1):
+            mapping[i, curr_k] = np.logaddexp(consts2[i] + mapping[i + d, curr_k - 1], mapping[i + 1, curr_k])
+            mapping[i, curr_k] = np.logaddexp(consts1[i] + consts2[i] + g[i + d, curr_k - 1], mapping[i, curr_k])
+
+    return np.exp(mapping[0, k] - g[0, k]) - k * r
+
+
+def gradient_descent(F_F_tag, initial_x, t=0.01, epsilon=1e-10, max_iter=200, concave=False):
+    x_prev = initial_x
+    F_prev, F_tag_prev = F_F_tag(x_prev)
+    for i in range(max_iter):
+        x_current = x_prev + t * F_tag_prev if concave else x_prev - t * F_tag_prev
+        F_current, F_tag_current = F_F_tag(x_current)
+        if np.abs(F_current - F_prev) < epsilon:
+            break
+        t = np.abs((x_current - x_prev) / (F_tag_prev - F_tag_current))
+        x_prev, F_prev, F_tag_prev = x_current, F_current, F_tag_current
+    return F_current, x_current
+
+
+# Code for 1d optimization
 def max_argmax_1d_case(y, filt, k, noise_std, x_0=1, t=0.01, epsilon=1e-5, max_iter=100):
     # If got only one y
     if not hasattr(y[0], '__iter__'):
@@ -270,18 +374,20 @@ def max_argmax_1d_case(y, filt, k, noise_std, x_0=1, t=0.01, epsilon=1e-5, max_i
         k = [k] * len(y)
 
     def F_F_tag(x):
-        return f_f_tag_many(x, y, filt, k, noise_std)
+        f = 0
+        f_tag = 0
+        for y_, k_ in zip(y, k):
+            f_, f_tag_ = f_f_tag(x, y_, filt, k_, noise_std)
+            f += f_
+            f_tag += f_tag_
+        return f, f_tag
 
-    return gradient_descent(F_F_tag, x_0, t, epsilon, max_iter)
+    # Works a little bit slower
+    # def F_F_tag(x):
+    #     return f_f_tag_many(x, y, filt, k, noise_std)
+    return gradient_descent(F_F_tag, x_0, t, epsilon, max_iter, concave=True)
 
 
-def precompute_f_f_tag(power, mgraph, filt, noise_std):
-    x_tag = np.flip(filt)  # Flipping to cross-correlate
-    conv = convolve(mgraph, x_tag, mode='valid') / (noise_std ** 2)
-    return conv, conv * power
-
-
-@nb.jit
 def f_f_tag(curr_power, y, x, k, sigma):
     n = y.shape[0]
     d = x.shape[0]
@@ -291,30 +397,52 @@ def f_f_tag(curr_power, y, x, k, sigma):
 
     const1, const2 = precompute_f_f_tag(curr_power, y, x, sigma)
     g = dynamic_programming_1d(n, k, d, const2)
-
-    r = - np.min(const1) + 10
-    const1 = np.log(const1 + r)
-
-    mapping = np.full((n + 1, k + 1), -np.inf)
-    mapping[:, 0] = 0
-
-    # Filling values one by one, skipping irrelevant values
-    # We already filled values when k=0 (=0) and when i>n-k*d
-    for curr_k in range(1, k + 1):
-        for i in range(n - curr_k * d, -1, -1):
-            mapping[i, curr_k] = np.logaddexp(const2[i] + mapping[i + d, curr_k - 1], mapping[i + 1, curr_k])
-            mapping[i, curr_k] = np.logaddexp(const1[i] + const2[i] + g[i + d, curr_k - 1], mapping[i, curr_k], )
+    dp_derivative = dynamic_programming_1d_derivative(n, k, d, const1, const2, g)
 
     log_f = log_c - k * np.sum(np.square(x)) * curr_power ** 2 / (2 * sigma ** 2) + g[0, k]
-    f_tag = - k * np.sum(np.square(x)) * curr_power / sigma ** 2 + np.exp(mapping[0, k] - g[0, k]) - k * r
+    f_tag = - k * np.sum(np.square(x)) * curr_power / sigma ** 2 + dp_derivative
+
     return log_f, f_tag
 
 
 def f_f_tag_many(curr_power, y, x, k, sigma):
-    f = 0
-    f_tag = 0
-    for y_, k_ in zip(y, k):
-        f_, f_tag_ = f_f_tag(curr_power, y_, x, k_, sigma)
-        f += f_
-        f_tag += f_tag_
-    return f, f_tag
+    n = y.shape[1]
+    d = x.shape[0]
+
+    const1, const2 = precompute_f_f_tag(curr_power, y, x, sigma)
+    g = dynamic_programming_1d_many(n, max(k), d, const2)
+
+    dp_derivative = dynamic_programming_1d_many_derivative(n, k, d, const1, const2, g)
+
+    # Computing constant part
+    log_c = np.array([-np.sum(np.square(y[i])) / (2 * sigma ** 2) - np.log(np.sqrt(2 * np.pi * sigma ** 2)) for i in range(len(y))])
+    tmp = np.array([-k[i] * np.sum(np.square(x)) * curr_power ** 2 / (2 * sigma ** 2) for i in range(len(y))])
+    g = np.array([g[i, 0, k[i]] for i in range(len(y))])
+    log_f = log_c + tmp + g
+
+    tmp = np.array([-k[i] * np.sum(np.square(x)) * curr_power / sigma ** 2 for i in range(len(y))])
+
+    f_tag = tmp + dp_derivative
+    return np.sum(log_f), np.sum(f_tag)
+
+
+# Code for 2d optimization
+def max_argmax_2d_case(y, filt, k, noise_std, x_0=1, t=0.01, epsilon=1e-5, max_iter=100):
+    # If got only one y
+    if not hasattr(y[0], '__iter__'):
+        y = [y]
+
+    # If got only one k
+    if not hasattr(k, '__iter__'):
+        k = [k] * len(y)
+
+    def F_F_tag(x):
+        f = 0
+        f_tag = 0
+        for y_, k_ in zip(y, k):
+            f_, f_tag_ = f_f_tag(x, y_, filt, k_, noise_std)
+            f += f_
+            f_tag += f_tag_
+        return f, f_tag
+
+    return gradient_descent(F_F_tag, x_0, t, epsilon, max_iter)
