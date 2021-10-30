4143f511803532d8a6f652a5518d7581dbe9cae5
Tamir <tamirshashafb@gmail.com>
2021-10-30 10:07:24 +0300

    fixed num exp

--- src/experiments/baselines/num_of_instances_experiment.py
+++ src/experiments/baselines/num_of_instances_experiment.py
@@ -1,17 +1,19 @@
+import os
+
+from src.constants import ROOT_DIR
 from src.experiments.data_simulator_2d import DataSimulator2D, Shapes2D
 from src.experiments.experiments_2d import Experiment2D, EstimationMethod
 
 NOISE_MEAN = 0
-NOISE_STD = 10
+NOISE_STD = 8
 LENGTH_OPTIONS = [20, 40, 60, 80, 100, 120, 140, 160]
-SIGNAL_SHAPES = [Shapes2D.disk,
-                 Shapes2D.sphere,
-                 lambda l, p: Shapes2D.ellipse(l, l // 1.7, p),
-                 lambda l, p: Shapes2D.double_disk(l, l // 2, p, 0)]
-sizes = [40, 80, 120]
+SIGNAL_SHAPES = [(Shapes2D.disk, 'disk'),
+                 (Shapes2D.sphere, 'sphere'),
+                 (lambda l, p: Shapes2D.ellipse(l, l // 1.7, p), 'ellipse'),
+                 (lambda l, p: Shapes2D.double_disk(l, l // 2, p, 0), 'ring')]
+sizes = [40, 60, 80, 100, 120]
 
-for i, signal_shape in enumerate(SIGNAL_SHAPES):
-    print(f'At signal shape #{i + 1}')
+for i, (signal_shape, shape_name) in enumerate(SIGNAL_SHAPES):
     for signal_size in sizes:
         data_simulator = DataSimulator2D(rows=1000,
                                          columns=1000,
@@ -22,12 +24,18 @@ for i, signal_shape in enumerate(SIGNAL_SHAPES):
                                          noise_std=NOISE_STD,
                                          noise_mean=NOISE_MEAN,
                                          apply_ctf=False)
+
         Experiment2D(
-            name=f"baseline_disk",
+            name=f"{shape_name}_{signal_size}",
             simulator=data_simulator,
             estimation_method=EstimationMethod.VeryWellSeparated,
+            estimate_noise=True,
             length_options=LENGTH_OPTIONS,
             fixed_num_of_occurrences=int(0.8 * data_simulator.occurrences),
             plot=False,
             save=True,
+            save_dir=os.path.join(ROOT_DIR, f'src/experiments/baselines/plots/fixed_num_of_instances/')
         ).run()
+
+
+
