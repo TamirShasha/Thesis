2bfb749dd33ab16322d3ffb944672420bd7cca4d
Tamir <tamirshashafb@gmail.com>
2021-12-28 08:22:44 +0200

    baselines

--- src/experiments/baselines/vws_baselines.py
+++ src/experiments/baselines/vws_baselines.py
@@ -8,19 +8,20 @@ from src.experiments.experiments_2d import Experiment2D, EstimationMethod
 
 NOISE_MEAN = 0
 NOISE_STD = 8
-LENGTH_OPTIONS = np.array([20, 40, 60, 80, 100, 120, 140, 160])
+N = 2000
+LENGTH_OPTIONS_PERC = np.array([2, 4, 6, 8, 10, 12, 14, 16])
 SIGNAL_SHAPES = [(Shapes2D.disk, 'disk'),
                  (Shapes2D.sphere, 'sphere'),
                  (lambda l, p: Shapes2D.ellipse(l, l // 1.7, p), 'ellipse'),
                  (lambda l, p: Shapes2D.double_disk(l, l // 2, p, 0), 'ring')]
-sizes = [40, 60, 80, 100, 120]
+sizes = np.array([4, 6, 8, 10, 12]) * N / 100
 
 now_str = datetime.now().strftime("%H_%M_%Y_%m_%d")
 
 for i, (signal_shape, shape_name) in enumerate(SIGNAL_SHAPES):
     for signal_size in sizes:
-        data = DataSimulator2D(rows=1000,
-                               columns=1000,
+        data = DataSimulator2D(rows=N,
+                               columns=N,
                                signal_length=signal_size,
                                signal_power=1,
                                signal_fraction=1 / 6,
@@ -33,10 +34,11 @@ for i, (signal_shape, shape_name) in enumerate(SIGNAL_SHAPES):
             name=f"{shape_name}_{signal_size}",
             simulator=data,
             estimation_method=EstimationMethod.VeryWellSeparated,
-            signal_length_by_percentage=LENGTH_OPTIONS / 10,
+            signal_length_by_percentage=LENGTH_OPTIONS_PERC,
             num_of_instances_range=(50, 150),
             estimate_noise=True,
             filter_basis_size=7,
+            particles_margin=0.02,
             save_statistics=True,
             plot=False,
             save=True,
